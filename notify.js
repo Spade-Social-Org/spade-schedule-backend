d1d027a62baef943bcdf2d8784bce9f3fe00ceac
supreme2580 <victoromorogbe69@gmail.com>
2023-12-13 23:10:33 +0100

    added notifications

--- notify.js
+++ notify.js
@@ -1,6 +1,13 @@
 const axios = require("axios");
 
-async function Notify(token, inviter, place, date_id, user_date_id, user_id) {
+export async function Notify(
+  token,
+  inviter,
+  place,
+  date_id,
+  user_date_id,
+  user_id
+) {
   const response = await axios.post(
     "https://spade-backend-v3-production.up.railway.app/api/v1/notifications",
     {
